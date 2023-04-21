7b8902dc89989be0fb21c99e3f35dc0de22b8454
LMehta0210 <lmehta@guilford.edu>
2023-04-21 09:53:32 -0400

    reformatted output

--- src/main/java/edu/guilford/App.java
+++ src/main/java/edu/guilford/App.java
@@ -18,25 +18,37 @@ public class App
         System.out.println("r2: " + r2);
         
         //tests negate method
+        System.out.println("\nTest negate method");
+        System.out.println("Input 3/4, Expected output -3/4");
+        System.out.print("Negated " + r2 + ": ");
         r2.negate();
-        System.out.println("Negated r2: " + r2);
+        System.out.println(r2);
         r2.negate();
 
         //test invert method
+        System.out.println("\nTest invert method");
+        System.out.println("Input 3/4, Expected output 4/3");
+        System.out.print("Inverted  q" + r2 + ": ");
         r2.invert();
-        System.out.println("Inverted r2: " + r2);
+        System.out.println(r2);
         r2.invert();
 
         //test toDouble method
-        System.out.println("Double r2: " + r2.toDouble());
+        System.out.println("\nTest toDouble method");
+        System.out.println("Input 3/4, Expected output .75");
+        System.out.println("Double " + r2 + ": " + r2.toDouble());
 
         //test reduce method
+        System.out.println("\nTest reduce method");
         Rational r3 = new Rational(12, 18);
-        System.out.println("r3: " + r3);
+        System.out.println("Input 12/18, Expected output 2/3");
+        System.out.print("Reduced " + r3 + ": ");
         r3.reduce();
-        System.out.println("Reduced r3: " + r3);
+        System.out.println(r3);
 
         //test add method
+        System.out.println("\nTest add method");
+        System.out.println("Input 3/4 and 2/3, Expected output 17/12");
         System.out.println(r2 + " + " + r3 + " = " + r2.add(r3));
     }
 }
